c54fcdc40a8ed0fb74ab33dc870b973d8866b9ea
XtremeYZ <90326810+XtremeYZ@users.noreply.github.com>
2021-09-29 11:07:25 +0200

    exercieTD4fini

--- TD1Exo4.c
+++ TD1Exo4.c
@@ -31,13 +31,54 @@ for (int i = 0; i < 10; i++)
 }
 }
 
+int moyenne(Tab tab) 
+{
+int somme=0,moyenne=0,nombre=0;
+for (int i = 0; i < 10; i++)
+{
+    somme= somme + tab[i];
+    nombre++;
+}
+ moyenne = somme/ nombre;
+return moyenne;
+}
+
+void MaxMin (Tab tab) 
+{
+int min=99999999999999,max = 0,position=0, longueur=10;
+        for (int i=0 ; i< longueur ; i++)
+      {
+          if(tab[i]<min){
+                         min=tab[i];
+                         position=i;
+          }
+      }
+        for (int i=0 ; i< longueur ; i++)
+      {
+          if(tab[i]>max){
+                         max=tab[i];
+                         position=i;
+          }
+      }
+int MinMax[2]= {max, min};
+printf("la valeur maximal est = %d et la valeur minimal est = %d",MinMax[0], MinMax[1]);
+}
+
 int main(void)
 {
 Tab tab1[]= {12,-8,254,11,5744,3,};
 Tab tab2[]= {0};
+Tab MinMax[2];
+int moyennes, positionMin;
 
 recopieTableau(tab1,tab2);
 afficher(tab1);
 zero(tab2);
+moyennes= moyenne(tab1);
+printf("moyenne est: %d\n", moyennes);
+positionMin= posmin(tab1);
+printf("\nl'indice de la valeur min est %d\n", positionMin);
+ValeurMaxMin(tab1);
 
 return 0; 
+
